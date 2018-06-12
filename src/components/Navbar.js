059e96d3fe9a7e85fe4fca301fb06d40b21732e6
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-06-12 12:45:23 -0400

    use flexbox for navbar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = ({path}) => {
   var navbarContents
   switch(path) { // This is clunky, but it seems to be the best way to do route-specific rendering here without swapping out the navbar entirely.
     case '/splash':
-      navbarContents = (<div className="splash-navbar">
-        <div className="img">img goes here</div>
-        <div className="enter-site">Enter Site</div>
-      </div>)
+      navbarContents = (<ul className="splash-navbar">
+        <li className="img">img goes here</li>
+        <li className="enter-site"><p>Enter Site</p></li>
+      </ul>)
       break;
     default:
       navbarContents = <div>hello world</div>
