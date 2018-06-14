6ad9f24f100a21588f84c5943ab74655fe14d919
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-06-14 13:10:51 -0400

    issue admin

--- src/templates/candidate-fragment.js
+++ src/templates/candidate-fragment.js
@@ -1,7 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import Link from "gatsby-link";
 
 export const CandidateFragmentTemplate = () => <div />;
 
