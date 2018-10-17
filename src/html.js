24417413a9a4bec1411551f95b5009fcc1fbc5d2
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-10-17 12:01:59 -0400

    add gsite verification

--- src/html.js
+++ src/html.js
@@ -26,6 +26,7 @@ module.exports = class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="google-site-verification" content="-jhrvT598jaA36zbZ6wjXo04JLa705-J9L8jm_GNE2s" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
