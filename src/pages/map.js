31c3cca5467204a8fa96deb370a9408663503b8c
Rapi Castillo <rapi.castillo@gmail.com>
2018-11-12 23:38:09 -0500

    Clear Map

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MapboxGL from 'mapbox-gl';
-import ReactMapboxGl, { LngLatBounds, ScaleControl, ZoomControl, Layer, 
+import ReactMapboxGl, { LngLatBounds, ScaleControl, Marker, ZoomControl, Layer, 
                         Feature, Source, GeoJSONLayer } from 'react-mapbox-gl';
 import "../style/map.scss";
 
@@ -25,25 +25,31 @@ export default class MapPage extends React.Component {
 
         map.setCenter({lng: -95.7129, lat: 37.0902});
         map.setZoom(4);
-        map.on('mousemove', 'cd-fill', (e) => {
-
-        });
 
         map.on('mousemove', 'states-fill', e => {
             const stateProp =  e.features[0].properties;
-            // Set data to highlight State
 
-            // console.log("Testing ::", this.state.selectedState, stateProp.STATE, this.selectedState !== stateProp.STATE);
+            // Set data to highlight State
             const relatedFeatures = this.state.selectedState !== stateProp.STATE ? map.querySourceFeatures('states', {
                 sourceLayer: 'gz_2010_us_040_00_5m-81sosm',
                 filter: ['==', 'STATE', stateProp.STATE]
             }) : null;
-
-            // console.log(relatedFeatures);
             this.setState({ hoveredState: relatedFeatures });
-
-            // console.log("Hovered State:: ", this.state.hoveredState, " Selected State:: ", this.state.selectedState );
         });
+
+        map.on('mousemove', 'cd-fill', e => {
+            const stateProp =  e.features[0].properties;
+
+            // Set data to highlight State
+            const relatedFeatures = this.state.selectedState == stateProp.STATEFP ? map.querySourceFeatures('congressional-districts', {
+                sourceLayer: 'cd-0ayx0b',
+                filter: ['all',
+                            ['==', 'CD116FP', stateProp.CD116FP],
+                            ['==', 'STATEFP', stateProp.STATEFP]
+                        ]
+            }) : null;
+            this.setState({ hoveredCD: relatedFeatures });
+        })
 
         // Select a state to focus
         map.on('click', 'states-fill', e => {
@@ -61,31 +67,39 @@ export default class MapPage extends React.Component {
                 ['==', ['get', 'STATEFP'], `${stateProp.STATE}`], 1,
                 0
             ]);
+            map.setPaintProperty('cd-fill', 'fill-opacity', ['case', 
+                ['==', ['get', 'STATEFP'], `${stateProp.STATE}`], 0.1,
+                0
+            ]);
 
-            // Zoom in to State
-            let coordinates = e.features[0].geometry.coordinates;
-            if (coordinates.length > 1) {
-                let newCoord = [];    
-                coordinates.forEach(coordSet => {
-                    newCoord = newCoord.concat(coordSet[0]);
-                    console.log("New Coords", coordSet[0], newCoord);
-                });
-                coordinates = newCoord;
-            } else {
-                coordinates = coordinates[0];
+           const bounds = this.getBounds(e.features[0].geometry);
+            if(map.getZoom() <= 5) {
+                map.fitBounds(bounds, { padding: 100, animate: false });
             }
-            
-            const init = coordinates[0];
-            const bounds = coordinates.reduce(function (bounds, coord) {
-                return bounds.extend(coord);
-            }, new MapboxGL.LngLatBounds(init, init))
-            
-            map.fitBounds(bounds, { padding: 100 });
-
         });
 
-        
     } 
+
+    getBounds(geom) {
+         // Zoom in to State
+         let coordinates = geom.coordinates;
+         if (coordinates.length > 1) {
+             let newCoord = [];    
+             coordinates.forEach(coordSet => {
+                 newCoord = newCoord.concat(coordSet[0]);
+             });
+             coordinates = newCoord;
+         } else {
+             coordinates = coordinates[0];
+         }
+         
+         const init = coordinates[0];
+         const bounds = coordinates.reduce(function (bounds, coord) {
+             return bounds.extend(coord);
+         }, new MapboxGL.LngLatBounds(init, init))
+         
+         return bounds;
+    }
 
     render() {
         return (
@@ -99,7 +113,6 @@ export default class MapPage extends React.Component {
                         width: "100%",        
                     }}
                     onMouseMove={(map, evt) => { 
-                        // console.log(map, evt) 
                         }
                     }
 
@@ -110,10 +123,11 @@ export default class MapPage extends React.Component {
                     {/* Congressional Districts */}
                     <Source 
                         id="congressional-districts"
-                    tileJsonSource={{
-                        "type": "vector", 
-                        "url": "mapbox://rcscastillo.5r1x01zm"
-                    }}/>
+                        tileJsonSource={{
+                            "type": "vector", 
+                            "url": "mapbox://rcscastillo.5r1x01zm"
+                        }}
+                    />
                     <Layer
                         type='line'
                         id='cd-line'
@@ -128,6 +142,16 @@ export default class MapPage extends React.Component {
                             'visibility': 'visible'
                         }}
                     ></Layer>
+                    <Layer
+                        type='fill'
+                        id='cd-fill'
+                        sourceId={'congressional-districts'}
+                        sourceLayer={'cd-0ayx0b'}
+                        before={'waterway-label'}
+                        paint={{
+                            'fill-opacity': 0
+                        }}
+                    ></Layer>
 
                     {/* States */}
                     <Source
@@ -136,7 +160,6 @@ export default class MapPage extends React.Component {
                             "type": "vector", 
                             "url": "mapbox://rcscastillo.25eloeq2"
                         }} />
-
                     <Layer
                         type='fill'
                         id='states-fill'
@@ -149,7 +172,7 @@ export default class MapPage extends React.Component {
                         }}
                     ></Layer>
 
-                     {/* Hover */}
+                    {/* Hover */}
                     <Layer
                         type='fill'
                         id='states-hover-layer'
@@ -162,7 +185,24 @@ export default class MapPage extends React.Component {
                         {this.state.hoveredState && this.state.hoveredState.map(state => <Feature coordinates={state.geometry.coordinates}/>)}
                     </Layer>
 
-                    
+                    <Layer
+                        type='fill'
+                        id='cd-hover-layer'
+                        before={'cd-line'}
+                        paint={{
+                            'fill-color': '#FAFAFA',
+                            'fill-opacity': 1
+                        }}
+                        >
+                        {this.state.hoveredCD && this.state.hoveredCD.map(cd => <Feature coordinates={cd.geometry.coordinates}/>)}
+                    </Layer>
+
+
+                    <Marker
+                        coordinates={[-95.7129, 37.0902]}
+                        anchor="bottom">
+                        <div style={{backgroundColor: 'white', padding: 20}}> Hello world How are you?</div>
+                    </Marker>
                 </Map>
             </section>
             <section className='activity-area'>
@@ -170,27 +210,3 @@ export default class MapPage extends React.Component {
         </div>);
     }
 }
-
-
-
-// {/* Activated CDs */}
-// <Layer
-//  type='fill'
-//  id='active-state-cd-fill'
-//  paint={{
-//      'fill-color': 'blue',
-//      'fill-opacity': 0.1,
-//  }}
-// >
-//  {this.state.stateCDs && this.state.stateCDs.map(cd => <Feature coordinates={cd.geometry.coordinates}/>)}
-// </Layer>
-// <Layer
-//  type='line'
-//  id='active-state-cd-line'
-//  paint={{
-//      'line-width': 2,
-//      'line-color': 'black'
-//  }}
-// >
-//  {this.state.stateCDs && this.state.stateCDs.map(cd => <Feature coordinates={cd.geometry.coordinates}/>)}
-// </Layer>
