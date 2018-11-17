c53c3f63f6e14aae40329c3f2c4039075594a7d6
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-11-17 18:04:56 -0500

    add meta og image

--- src/html.js
+++ src/html.js
@@ -34,6 +34,10 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta
+            property="og:image"
+            content="https://www.justicedemocrats.com/img/jd_site_home_cr2020_background_1920x1980_test.jpg"
+          />
           <link
             href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,800"
             rel="stylesheet"
