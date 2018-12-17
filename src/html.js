2a4a674d5c95284c08d68df33008fbe8f50a334c
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-12-17 12:18:23 -0500

    temp change meta image

--- src/html.js
+++ src/html.js
@@ -1,45 +1,39 @@
-import React from 'react'
+import React from "react";
 
-let stylesStr
+let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
+    stylesStr = require(`!raw-loader!../public/styles.css`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 module.exports = class HTML extends React.Component {
   render() {
-    let css
+    let css;
     if (process.env.NODE_ENV === `production`) {
       css = (
         <style
           id="gatsby-inlined-css"
           dangerouslySetInnerHTML={{ __html: stylesStr }}
         />
-      )
+      );
     }
 
     const simpleMetas = [
       [
-        'google-site-verification',
-        '-jhrvT598jaA36zbZ6wjXo04JLa705-J9L8jm_GNE2s',
+        "google-site-verification",
+        "-jhrvT598jaA36zbZ6wjXo04JLa705-J9L8jm_GNE2s"
       ],
-      ['viewport', 'width=device-width, initial-scale=1, shrink-to-fit=no'],
-      [
-        'og:image',
-        'https://www.justicedemocrats.com/img/jd_site_home_cr2020_background_1920x1980_test.jpg',
-      ],
-      [
-        'twitter:image',
-        'https://www.justicedemocrats.com/img/jd_site_home_cr2020_background_1920x1980_test.jpg',
-      ],
-      ['twitter:card', 'summary_large_image'],
-      ['og:title', "Justice Democrats | It's #OurTime"],
-      ['twitter:title', "Justice Democrats | It's #OurTime"],
-      ['og:text', 'Justice Democrats FTW'],
-    ]
+      ["viewport", "width=device-width, initial-scale=1, shrink-to-fit=no"],
+      ["og:image", "https://www.justicedemocrats.com/img/temp-meta.png"],
+      ["twitter:image", "https://www.justicedemocrats.com/imgtemp-meta.png"],
+      ["twitter:card", "summary_large_image"],
+      ["og:title", "Justice Democrats | It's #OurTime"],
+      ["twitter:title", "Justice Democrats | It's #OurTime"],
+      ["og:text", "Justice Democrats FTW"]
+    ];
 
     return (
       <html {...this.props.htmlAttributes}>
@@ -74,7 +68,7 @@ module.exports = class HTML extends React.Component {
                 <input type="email" name="email" required="true" />
                 <input type="text" maxlength="5" minlength="5" name="zip" required="true" />
                 <input name="phone" type="tel" required="true" />
-              </form>`,
+              </form>`
             }}
           />
           <div
@@ -87,6 +81,6 @@ module.exports = class HTML extends React.Component {
           <script>window.request = window.superagent;</script>
         </body>
       </html>
-    )
+    );
   }
-}
+};
