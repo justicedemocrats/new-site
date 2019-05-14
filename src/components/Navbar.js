4e4f4d64dfc1c93e7891b0d7129024c6d46f1e56
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-05-14 11:37:05 -0400

    add candidates and nominate dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,46 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import PropTypes from 'prop-types'
-import '../style/navbar.scss'
-import Responsive from 'react-responsive'
-import { slide as Menu } from 'react-burger-menu'
+import React from "react";
+import Link from "gatsby-link";
+import PropTypes from "prop-types";
+import "../style/navbar.scss";
+import Responsive from "react-responsive";
+import { slide as Menu } from "react-burger-menu";
 
 // const DesktopOrTablet = props => <Responsive {...props} minWidth={992} />;
 // const Mobile = props => <Responsive {...props} minWidth={992} />;
 // const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
-const Mobile = props => <Responsive {...props} maxWidth={767} />
-const Default = props => <Responsive {...props} minWidth={768} />
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
+const Default = props => <Responsive {...props} minWidth={768} />;
 
 const links = [
-  { display: 'Home', href: '/home' },
-  { display: 'About', href: '/about' },
-  { display: 'Platform', href: '/issues' },
-  { display: 'Nominate', href: '/nominate' },
+  { display: "Home", href: "/home" },
+  { display: "About", href: "/about" },
+  { display: "Platform", href: "/issues" },
+  {
+    display: "Candidates",
+    href: "/candidates",
+    children: [
+      { display: "Nominate", href: "/nominate" },
+      { display: "2020 Candidates", href: "/candidates" }
+    ]
+  },
   // { display: 'News', href: '/news'},
   // { display: 'Actions', href: '/actions'},
-  { display: 'Store', href: 'https://shop.justicedemocrats.com' },
-  { display: 'Join', href: '/' },
-]
+  { display: "Store", href: "https://shop.justicedemocrats.com" },
+  { display: "Join", href: "/" }
+];
 
 class Navbar extends React.Component {
-  state = { open: false }
+  state = { open: false };
 
-  handleMenuChange = ({ isOpen }) => this.setState({ open: isOpen })
-  closeMenu = () => this.setState({ open: false })
+  handleMenuChange = ({ isOpen }) => this.setState({ open: isOpen });
+  closeMenu = () => this.setState({ open: false });
 
   render() {
-    const { path } = this.props
+    const { path } = this.props;
 
-    if (path == '/' || path == '') {
+    if (path == "/" || path == "") {
       return (
-        <div className="navbar" style={{ osition: 'fixed' }}>
+        <div className="navbar" style={{ osition: "fixed" }}>
           <div className="navbar desktop">
             <Default>
               <div className="nav-social-container">
@@ -46,28 +53,28 @@ class Navbar extends React.Component {
               to="/home"
               style={{
                 width: 150,
-                display: 'block',
-                textDecoration: 'none',
-                color: 'white',
+                display: "block",
+                textDecoration: "none",
+                color: "white",
                 padding: 10,
-                textTransform: 'uppercase',
-                height: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginLeft: 10,
+                textTransform: "uppercase",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginLeft: 10
               }}
               className="orange-bg bold-m"
             >
-              <div style={{ textAlign: 'center' }}> Enter Site </div>
+              <div style={{ textAlign: "center" }}> Enter Site </div>
             </Link>
           </div>
         </div>
-      )
+      );
     }
 
     return (
-      <div className="navbar" style={{ position: 'fixed' }}>
+      <div className="navbar" style={{ position: "fixed" }}>
         <Default>
           <div className="hanger-navbar-container">
             <div className="navbar desktop">
@@ -81,15 +88,15 @@ class Navbar extends React.Component {
                 target="_blank"
                 style={{
                   width: 150,
-                  display: 'block',
-                  textDecoration: 'none',
-                  color: 'white',
+                  display: "block",
+                  textDecoration: "none",
+                  color: "white",
                   padding: 10,
-                  textTransform: 'uppercase',
-                  height: '100%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
+                  textTransform: "uppercase",
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
                 }}
                 className="light-blue-bg bold-m"
               >
@@ -97,23 +104,21 @@ class Navbar extends React.Component {
               </a>
             </div>
             <div className="navbar-hanger">
-              {links.map(
-                ({ display, href }) =>
-                  (href.startsWith('https://')
-                    ? <a
-                        className="hanger-link bold-m"
-                        href={href}
-                        target="_blank"
-                      >
-                        <div> {display} </div>
-                      </a>
-                    : <Link
-                        className={`hanger-link bold-m ${display.toLowerCase()}`}
-                        to={href}
-                      >
-                        <div> {display} </div>
-                      </Link>)
-              )}
+              {links.map(({ display, href, children }) => (
+                <div
+                  className={`hanger-link-container ${display.toLowerCase()}`}
+                >
+                  <DesktopNavLink display={display} href={href} />
+
+                  {(children || []).map((child, idx) => (
+                    <DesktopNavLink
+                      {...child}
+                      isChild={true}
+                      heightOffset={(idx + 1) * 50}
+                    />
+                  ))}
+                </div>
+              ))}
             </div>
           </div>
         </Default>
@@ -130,20 +135,20 @@ class Navbar extends React.Component {
               onStateChange={this.handleMenuChange}
               isOpen={this.state.open}
             >
-              {links.map(({ display, href }) => (
+              {links.map(({ display, href, children }) => (
                 <div onClick={this.closeMenu}>
-                  {/* <Link className="bold-m" to={href}>
-                    {display}
-                  </Link> */}
-                  {href.startsWith('https://')
-                    ? <a className="bold-m" href={href} target="_blank">
-                        <div> {display} </div>
-                      </a>
-                    : <Link className="bold-m" to={href}>
-                        <div> {display} </div>
-                      </Link>}
+                  <MobileNavLink display={display} href={href} />
+
+                  {(children || []).map(child => (
+                    <MobileNavLink
+                      display={child.display}
+                      href={child.href}
+                      isChild={true}
+                    />
+                  ))}
                 </div>
               ))}
+
               <div onClick={this.closeMenu}>
                 <a
                   className="bold-m"
@@ -161,15 +166,15 @@ class Navbar extends React.Component {
           <div />
         </Default>
       </div>
-    )
+    );
   }
 }
 
 Navbar.propTypes = {
-  path: PropTypes.string.isRequired,
-}
+  path: PropTypes.string.isRequired
+};
 
-export default Navbar
+export default Navbar;
 
 const TwitterButton = () => (
   <a
@@ -179,7 +184,7 @@ const TwitterButton = () => (
   >
     <img src="/assets/twitter.svg" alt="twitter" />
   </a>
-)
+);
 const FacebookButton = () => (
   <a
     className="nav-social"
@@ -188,13 +193,13 @@ const FacebookButton = () => (
   >
     <img src="/assets/facebook.svg" alt="facebook" />
   </a>
-)
+);
 
 const DonateLink = () => (
   <a href="/donate" target="_blank" className="bold-m light-blue-bg">
     Donate
   </a>
-)
+);
 const HeaderLogo = () => (
   <Link to="/home" style={{ height: 30 }} className="header-logo">
     <img
@@ -203,4 +208,38 @@ const HeaderLogo = () => (
       alt="Justice Democrats"
     />
   </Link>
-)
+);
+
+const DesktopNavLink = ({ href, display, isChild, heightOffset }) =>
+  href.startsWith("https://") ? (
+    <a
+      className={`hanger-link bold-m ${isChild ? "child" : ""}`}
+      href={href}
+      target="_blank"
+    >
+      <div style={{ width: "100%" }}> {display} </div>
+    </a>
+  ) : (
+    <Link
+      className={`hanger-link bold-m ${display.toLowerCase()} ${
+        isChild ? "child" : ""
+      }`}
+      to={href}
+    >
+      <div style={{ width: "100%" }}> {display} </div>
+    </Link>
+  );
+
+const MobileNavLink = ({ href, display, isChild }) => {
+  const className = isChild ? "medium-m font-size-16" : "bold-m";
+
+  return href.startsWith("https://") ? (
+    <a className={className} href={href} target="_blank">
+      <div> {display} </div>
+    </a>
+  ) : (
+    <Link className={className} to={href}>
+      <div> {display} </div>
+    </Link>
+  );
+};
